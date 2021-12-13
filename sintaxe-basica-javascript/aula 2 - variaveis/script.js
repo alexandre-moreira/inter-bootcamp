4772b1a01851fe79b6286ae871178933f267903e
Alexandre Moreira <a.moreira.de.souza@avanade.com>
2021-12-13 17:54:56 -0300

    Sintaxe basica - aula02

---
+++ sintaxe-basica-javascript/aula 2 - variaveis/script.js
@@ -0,0 +1,13 @@
+// tipos primitivos
+
+// boolean
+var vOuF = false;
+console.log(typeof(vOuF));
+
+// number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+// string
+var nome = 'Alexandre';
+console.log(typeof(nome))
